aa9c039098f6bc3c3b620d34aac0909ea13491b7
lvbealr <lvbealr@mail.ru>
2025-05-17 22:06:22 +0300

    realize creating nodes (codegen)

--- front-end/AST/include/AST.h
+++ front-end/AST/include/AST.h
@@ -6,12 +6,12 @@
 #include "binaryTree.h"
 #include "nameTable.h"
 
-static const size_t POISON_VALUE = -666;
+static const int POISON_VALUE = -666;
 
 enum class nodeType {
-    TERMINATOR           = 0
+    TERMINATOR           = 0,
     CONSTANT             = 1,
-    IDENTIFIER           = 2,
+    STRING               = 2,
     KEYWORD              = 3,
     FUNCTION_DEFINITION  = 4,
     PARAMETERS           = 5,
@@ -20,7 +20,7 @@ enum class nodeType {
 };
 
 union nodeData {
-    size_t  nameTableIndex;
+    size_t  nameTableIndex;         // identifier index in global name table
     Keyword keyword;
     int     number = POISON_VALUE;
 };
@@ -32,15 +32,114 @@ struct astNode {
     char    *file  = NULL;
 };
 
-// TODO
-#define CONST_(NUMBER)
-#define NAME_(INDEX)
-#define FUNCTION_DEFINITION_(LEFT, RIGHT, ID_INDEX)
-#define VARIABLE_DECLARATION_(LEFT, RIGHT, ID_INDEX)
-#define PARAMETERS_(LEFT, RIGHT)
-#define FUNCTION_CALL_(LEFT, RIGHT)
-#define TERMINATOR_()
-#define OPERATOR_SEPARATOR_(LEFT, RIGHT)
-#define PARAMETERS_SEPARATOR_(LEFT, RIGHT)
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+#define _CONST_(NUMBER)                                 \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::CONSTANT,                \
+             .data = {.number = NUMBER},                \
+             .line = context->currentLine               \
+            },                                          \
+        .left   = NULL,                                 \
+        .right  = NULL,                                 \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _NAME_(INDEX)                                   \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::STRING,                  \
+             .data = {.nameTableIndex = INDEX},         \
+             .line = context->currentLine               \
+            },                                          \
+        .left   = NULL,                                 \
+        .right  = NULL,                                 \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _FUNCTION_DEFINITION_(LEFT, RIGHT, ID_INDEX)    \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::FUNCTION_DEFINITION,     \
+             .data = {.nameTableIndex = ID_INDEX},      \
+             .line = context->currentLine               \
+            },                                          \
+        .left   = LEFT,                                 \
+        .right  = RIGHT,                                \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _VARIABLE_DECLARATION_(LEFT, RIGHT, ID_INDEX)   \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::VARIABLE_DECLARATION,    \
+             .data = {.nameTableIndex = ID_INDEX},      \
+             .line = context->currentLine               \
+            },                                          \
+        .left   = LEFT,                                 \
+        .right  = RIGHT,                                \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _PARAMETERS_(LEFT, RIGHT)                       \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::PARAMETERS},             \
+        .left   = LEFT,                                 \
+        .right  = RIGHT,                                \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _FUNCTION_CALL_(LEFT, RIGHT)                    \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::FUNCTION_CALL},          \
+        .left   = LEFT,                                 \
+        .right  = RIGHT,                                \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _TERMINATOR_()                                  \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::TERMINATOR},             \
+        .left   = NULL,                                 \
+        .right  = NULL,                                 \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _OPERATOR_SEPARATOR_(LEFT, RIGHT)               \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::STRING,                  \
+             .data = {.nameTableIndex = 0}              \
+            },                                          \
+        .left   = LEFT,                                 \
+        .right  = RIGHT,                                \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+#define _ARGUMENTS_SEPARATOR_(LEFT, RIGHT)              \
+    emplaceNode(node<astNode>                           \
+        {.data =                                        \
+            {.type = nodeType::STRING,                  \
+             .data = {.nameTableIndex = 1}              \
+            },                                          \
+        .left   = LEFT,                                 \
+        .right  = RIGHT,                                \
+        .parent = NULL                                  \
+        }                                               \
+    )
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
 
 #endif // AST_H_
